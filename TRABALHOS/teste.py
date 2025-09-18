10932059bd25cf53318d2e8de005a8fb653dd0aa
Gustavo Camargo <gustavo.camargo1005@gmail.com>
2025-09-18 20:13:18 -0300

    kmlkl

--- TRABALHOS/teste.py
+++ TRABALHOS/teste.py
@@ -1,5 +1,22 @@
-import random
-
-num = random.randint(1,3)
-
-print(num)
+num = int(input("digite um numero: "))
+cont = 0
+par = 0
+impar = 0
+maior = num
+menor = num
+while cont < 9:
+    if num % 2 == 0:
+        par += 1
+    else:
+        impar +=1
+    if num > maior:
+        maior = num
+    else:
+        maior = maior
+    if num < menor:
+        menor = num
+    else:
+        menor = menor
+    num = int(input("digite um numero: "))
+    cont += 1
+print(f"maior é {maior} menor {menor} impar{impar} par{par}")
